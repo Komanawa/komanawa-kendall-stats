dc0bc2f44912384e04d7afd3c9efe0d65fc4ac31
matt_dumont <hansonmcoombs@gmail.com>
2023-09-29 18:07:03 +1300

    finish timetest runs and estimate runtime function

--- src/kendall_stats/utils.py
+++ src/kendall_stats/utils.py
@@ -20,7 +20,7 @@ def estimate_runtime(npoints, func, plot=False):
     assert func in ['MannKendall', 'SeasonalKendall', 'MultiPartKendall_2part', 'SeasonalMultiPartKendall_2part',
                         'MultiPartKendall_3part', 'SeasonalMultiPartKendall_3part']
 
-    data = pd.read_csv(Path(__file__).parents[2].joinpath('tests/time_test_results.csv'), index_col=0)
+    data = pd.read_csv(Path(__file__).parent.joinpath('time_test_results.csv'), index_col=0)
     data.columns = [e.replace('_time_test','') for e in data.columns]
     use_data = data[func]
     lr = linregress(use_data.index, np.log10(use_data))
