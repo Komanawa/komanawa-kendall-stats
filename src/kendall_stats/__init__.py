56ed1f00fec0a16acefc8416f177b64d14e199d1
matt_dumont <hansonmcoombs@gmail.com>
2023-09-29 18:00:59 +1300

    finish timetest runs and estimate runtime function

--- src/kendall_stats/__init__.py
+++ src/kendall_stats/__init__.py
@@ -5,4 +5,4 @@ on: 21/09/23
 from kendall_stats.multi_part_kendall import MultiPartKendall, SeasonalMultiPartKendall
 from kendall_stats.mann_kendall import MannKendall, SeasonalKendall
 import kendall_stats.make_example_data as example_data
-from utils import estimate_runtime
+from kendall_stats.utils import estimate_runtime
