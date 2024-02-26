3e924739c151dceef5b0c9dd225e4fb95bd23f51
matt_dumont <hansonmcoombs@gmail.com>
2024-02-27 10:14:34 +1300

    make organisation namespace, debug

--- src/komanawa/kendall_stats/__init__.py
+++ src/komanawa/kendall_stats/__init__.py
@@ -2,7 +2,7 @@
 created matt_dumont 
 on: 21/09/23
 """
-from multi_part_kendall import MultiPartKendall, SeasonalMultiPartKendall
-from mann_kendall import MannKendall, SeasonalKendall
-import make_example_data as example_data
-from utils import estimate_runtime
+from komanawa.kendall_stats.multi_part_kendall import MultiPartKendall, SeasonalMultiPartKendall
+from komanawa.kendall_stats.mann_kendall import MannKendall, SeasonalKendall
+import komanawa.kendall_stats.make_example_data as example_data
+from komanawa.kendall_stats.utils import estimate_runtime
